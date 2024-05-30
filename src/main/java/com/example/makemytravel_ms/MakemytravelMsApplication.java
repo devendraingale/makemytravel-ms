2bec70f4185c55379a3db86de024737c157b27b9
Devendra <ingale.devendra20@gmail.com>
2024-05-30 09:56:38 +0530

    source code

---
+++ src/main/java/com/example/makemytravel_ms/MakemytravelMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.makemytravel_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakemytravelMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakemytravelMsApplication.class, args);
+	}
+
+}
