bf7fcc4a968e2a7195618877840859a40b08be35
Devendra <ingale.devendra20@gmail.com>
2024-06-18 10:06:24 +0530

    mono

--- src/main/java/com/example/makemytravel_ms/metro.java
+++ src/main/java/com/example/makemytravel_ms/metro.java
@@ -10,7 +10,7 @@ public class metro {
     @GetMapping("/mymetro")
     public String getData(){
 
-        return "Please book your metro ticket oo modeule";
+        return "Please book your metro ticket oo modeule to test weebhook";
     }
 }
 
